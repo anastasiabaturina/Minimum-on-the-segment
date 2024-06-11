108974870eb53e47571db2ab9cfbc8257e420c45
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-06-11 14:50:26 +0300

    решение задачи

---
+++ Minimum-on-the-segment/Program.cs
@@ -0,0 +1,9 @@
+﻿using Minimum_on_the_segment;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        SegmentMinimum.FindMinimumOnRange(9, 3, [1, 3, 2, 4, 5, 3, 0, 9, 7]);
+    }    
+}
---
+++ Minimum-on-the-segment/SegmentMinimum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Minimum_on_the_segment
+{
+    public class SegmentMinimum
+    {
+        public static void FindMinimumOnRange(int n, int k, int[] array)
+        {
+            List<int[]> numbers = new List<int[]>();
+
+            for (int i = 0; i < array.Length - k + 1; i++)
+            {
+                int[] number = new int[k];
+                Array.Copy(array, i, number, 0, k);
+                numbers.Add(number);
+            }
+
+            foreach (int[] num in numbers)
+            {
+                Console.WriteLine(num.Min());
+            }
+        }
+    }
+}
